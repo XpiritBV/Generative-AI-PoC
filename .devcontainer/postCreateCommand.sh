d21229e6ea39dfc1993ee42f2cdf45333f9d661f
Jeroen van de Kraats <J_v_d_K@hotmail.com>
2023-05-10 09:04:00 +0000

    Changes to devcontainer

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,5 @@
+pip install --user -r requirements.txt && 
+# Install Azure Functions Core Tools
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg && sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg &&
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/debian/$(lsb_release -rs | cut -d'.' -f 1)/prod $(lsb_release -cs) main" > /etc/apt/sources.list.d/dotnetdev.list' &&
+sudo apt-get update && sudo apt-get install azure-functions-core-tools-4
